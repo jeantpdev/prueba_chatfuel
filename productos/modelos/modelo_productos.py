d8c71dd0436aaab04a62c0102bd520084346aab3
Jean Trujillo <91339890+jeantpdev@users.noreply.github.com>
2025-01-21 18:46:36 -0500

    Add files via upload

--- productos/modelos/modelo_productos.py
+++ productos/modelos/modelo_productos.py
@@ -9,10 +9,11 @@ class Formulario():
             datos = request.get_json()
             print(datos)
             nombre = datos["nombre"]
+            print(nombre)
             return jsonify({"mensaje": "Datos recibidos correctamente", "nombre": nombre}), 200
 
             # return jsonify({"urls_imagenes_secundarias": urls_imagenes_secundarias, "url_imagen_principal": url_imagen_principal}), 200
         
         except Exception as e:
-            return jsonify({"mensaje": str(e)}), 500    
+            return jsonify({"mensaje": str(e)}), 500
        
